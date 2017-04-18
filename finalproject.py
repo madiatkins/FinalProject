4a17ad2e3e1e518053da289d50f8bfc9ad3e2bde
Madi Atkins <madiatk@umich.edu>
2017-04-18 15:54:48 -0400

    Wrote all three data fetching/caching functions

--- finalproject.py
+++ finalproject.py
@@ -6,12 +6,31 @@ import sqlite3
 import re
 import collections
 import itertools
-
+from itertools import chain
+import requests
+import csv
+import webbrowser
+from pprint import pprint
 import sys
 import codecs
-sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer) ##to deal with emojies and other languages
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer) 
+##to deal with emojies and other languages
+
+##Tweepy setup
+
+consumer_key = twitter_info.consumer_key
+consumer_secret = twitter_info.consumer_secret
+access_token = twitter_info.access_token
+access_token_secret = twitter_info.access_token_secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 
+## Insert 3 movie titles into movie list
+
+movie_titles = ["Hidden Figures", "Rogue One", "Lazer Team"]
+
+## Create initial cache setup
 
 CACHE_FNAME = "finalproject.json"
 # Put the rest of your caching setup here:
@@ -20,25 +39,26 @@ try:
 	cache_contents = cache_file.read()
 	cache_file.close()
 	CACHE_DICTION = json.loads(cache_contents)
+	
 except:
 	CACHE_DICTION = {}
 
 
-# Define your function get_user_tweets here:
+# Define your function get_user_tweets to fetch and cache tweets based on a Twitter screen name:
 
 def get_user_tweets(username):
-	twitter_name = "twitter_"+str(username)
+	twitter_phrase = "twitter_"+str(username)
 	tweets = []
 
-	if twitter_name in CACHE_DICTION:
+	if twitter_phrase in CACHE_DICTION:
 		print('using cache')
-		response_text = CACHE_DICTION[twitter_name]
-		for tweet in response_text[:21]:
+		response_text = CACHE_DICTION[twitter_phrase]
+		for tweet in response_text:
 			tweets.append(tweet)
 	else:
 		print('fetching')
 		public_tweets = api.user_timeline(id=username)
-		CACHE_DICTION[twitter_name] = public_tweets
+		CACHE_DICTION[twitter_phrase] = public_tweets
 		response_text = public_tweets
 
 		cache_file = open(CACHE_FNAME, 'w')
@@ -47,22 +67,22 @@ def get_user_tweets(username):
 
 		for tweet in response_text:
 			tweets.append(tweet)
-
 	return tweets
 
+## Define your functino get_tweets to fetch and cache tweets based on a Twitter phrase (in this case, movie titles):
 
 def get_tweets(phrase):
-	tweets = []
 	twitter_phrase = "twitter_"+str(phrase)
+	tweets = []
 
 	if twitter_phrase in CACHE_DICTION:
 		print('using cache')
 		response_text = CACHE_DICTION[twitter_phrase]
+
 	else:
 		print('fetching')
 		api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 		public_tweets = api.search(q=phrase)
-		
 		CACHE_DICTION[twitter_phrase] = public_tweets
 		response_text = public_tweets
 
@@ -70,117 +90,41 @@ def get_tweets(phrase):
 		cache_file.write(json.dumps(CACHE_DICTION))
 		cache_file.close()
 
+	# return response_text
 
-	for tweet in response_text:
+	tweeter = response_text["statuses"]
+	for tweet in tweeter:
 		tweets.append(tweet)
+	return tweets
 
 
+## Define your function get_OMDBdata to fetch and cache movie data based on a movie title:
 
 
+def get_OMDBdata(name):
+	baseurl = "http://www.omdbapi.com/?"
+	omdb_phrase = "omdbsearch_"+str(name)
 
+	if omdb_phrase in CACHE_DICTION:
+		print('using cache')
+		response_text = CACHE_DICTION[omdb_phrase]
 
+	else:
+		print('fetching')
+		omdb = requests.get(baseurl, params = {"t":name, "type":"movie"}).text
+		omdb_return = json.loads(omdb)
+		CACHE_DICTION[omdb_phrase] = omdb_return
+		response_text = omdb_return
+		cache_file = open(CACHE_FNAME, 'w')
+		cache_file.write(json.dumps(CACHE_DICTION))
+		cache_file.close()
 
+	return response_text
 
 
 
+## Define a class called "Movie" which should accept a dictionary that represents movie data from OMDB
+##It should have at least 3 instance variables and at least 2 methods besides the constructor
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##---------- TEST CASES ----------
-class CachingTests(unittest.TestCase):
-	def test_caching(self):
-		cache = open("finalproject.json","r").read()
-		self.assertTrue("Twilight" in cache, 'test to see that one of the movies titles I choose is in the cache')
-	def test_caching_type(self):
-		cache = open("finalproject.json", "r").read()
-		self.assertEqual(type(cache), type({}))
-
-class MovieTests(unittest.TestCase):
-	def test_movie1(self):
-		m = Movie()
-		self.assertEqual(type(m.title), type(""), 'Checking to make sure that the instance variable .title is a string')
-
-	def test_movie2(self):
-		m = Movie()
-		self.assertEqual(type(m.imdb_rating), type(1), 'testing that the imdb_rating instance variable is an integer')
-
-class TweetTests(unittest.TestCase):
-	def test_tweet1(self):
-		t = Tweet()
-		self.assertEqual(type(t.unique), type(""), "testing that the unique instance variable is a string type")
-
-class DatabaseTests(unittest.TestCase):
-	def test_db1(self):
-		conn = sqlite3.connect('finalproject.db')
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Users');
-		result = cur.fetchall()
-		self.assertTrue(len(result[0])==3,"Testing that there are 3 columns in the Users database")
-		conn.close()
-	def test_db2(self):
-		conn = sqlite3.connect('finalproject.db')
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Tweets');
-		result = cur.fetchall()
-		self.assertTrue(len(result[0])==6,"Testing that there are 6 columns in the Users database")
-		conn.close()
-	def test_db3(self):
-		conn = sqlite3.connect('project3_tweets.db')
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Movies');
-		result = cur.fetchall()
-		self.assertTrue(len(result[0])==6,"Testing that there are 6 columns in the Users database")
-		conn.close()
-
-
-
-
-
-
-if __name__ == "__main__":
-	unittest.main(verbosity=2)
